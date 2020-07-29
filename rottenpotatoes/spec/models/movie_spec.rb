b8d75514e61f936e7edf5d20a0ac0f45b5873ff0
Daniel Choi <djwc_0415@berkeley.edu>
2020-07-29 00:35:21 +0000

    Part 2 - waiting on help

---
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+before(:each) do
+  
+end
+
+describe Movie do
+  it 'should find movies by the same director' do
+    
+  end
+  
+  it 'should not find movies by different directors' do
+    
+  end
+end
