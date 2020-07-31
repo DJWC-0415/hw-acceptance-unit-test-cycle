288cccd71d31ef39c3ba2bc4723becfd5d1c20c5
Daniel Choi <djwc_0415@berkeley.edu>
2020-07-31 01:29:33 +0000

    Part 2 - managed to write the Movie rspec and add director finding functionality to Movie

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -3,6 +3,11 @@ class Movie < ActiveRecord::Base
     %w(G PG PG-13 NC-17 R)
   end
   
-  def self.movies_by_same_director director
+  def self.movies_by_same_director title
+    director = Movie.find_by(title: title).director
+    if director.blank?
+      return nil
+    end
+    return Movie.where(director: director).pluck(:title)
   end
 end
--- rottenpotatoes/spec/models/movie_spec.rb
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
-before(:each) do
-  
-end
-
 describe Movie do
-  it 'should find movies by the same director' do
-    
+  before(:each) do
+    @movie_1 = Movie.create(title: 'Interstellar', director: 'Christopher Nolan')
+    @movie_2 = Movie.create(title: 'Dunkirk', director: 'Christopher Nolan')
+    @movie_3 = Movie.create(title: 'Parasite', director: 'Bong Joon Ho')
+    @movie_4 = Movie.create(title: 'Everything Before Us')
   end
   
-  it 'should not find movies by different directors' do
-    
+  it 'should find movies by the same director' do
+    expect(Movie.movies_by_same_director(@movie_1.title)).to eql(['Interstellar', 'Dunkirk'])
+    expect(Movie.movies_by_same_director(@movie_2.title)).to eql(['Interstellar', 'Dunkirk'])
+    expect(Movie.movies_by_same_director(@movie_3.title)).to eql(['Parasite'])
+  end
+  
+  it 'should handle sad path when the movie has no director' do
+    expect(Movie.movies_by_same_director(@movie_4.title)).to eql(nil)
   end
 end
